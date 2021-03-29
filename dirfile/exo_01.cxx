e45deffafcfa7e271e349633512a36c61d504a58
Samy-Pelisson <Samy-Pelisson@users.noreply.github.com>
2021-03-29 07:58:42 +0200

    add files

---
+++ dirfile/exo_01.cxx
@@ -0,0 +1,147 @@
+
+
+#include <string.h>
+#include <exception>
+#include <iostream>   
+#include <sys/types.h>      // pid_t 
+#include <stdlib.h>
+
+#include <unistd.h>         // SEEK_SET
+#include <fcntl.h>          // O_RDWR
+#include <signal.h>
+
+
+#include "nsSysteme.h"
+
+#include "CExc.h"
+
+
+using namespace nsSysteme;
+using namespace nsFctShell;
+using namespace std;
+namespace{
+
+    
+//    void derout (int signum)  {
+//	SignalArrive = 1;
+//    }
+    
+ // Init la map ici
+ 
+ 
+ 
+ //Init les pipes
+ 
+ //retout sans erreur
+ void ResaPrise(int numSignal){
+ 	cout << "La reservation a bien été passée" << endl ;
+ }   
+// retour avec echec
+void ResaPleines(int numSignal){
+	cout << "La réservation a échouée pour manque de place" << endl;
+}
+
+// test string
+//bool validReservation(string requete){
+//	return true;
+//}
+
+
+}
+
+int main(int argc, char * argv []) {
+try{
+	
+
+	
+	
+
+ 	if (1 != argc) {
+    		throw CExc ("main()","Ce programme ne prend pas d'arguments");
+    	}
+    	
+	pid_t pidFils;
+	Signal(SIGUSR1, ResaPleines);
+	Signal(SIGUSR2, ResaPrise);
+
+	int   pipefd1[2]; // pipe de communication, pere parle
+	Pipe(pipefd1);
+
+	const int Nbuff =7; char requeteRecue[Nbuff];
+	
+	if((pidFils = Fork())){ // dans le pere
+		Close(pipefd1[0]);
+		cout << "dans papa" << endl;
+		
+		while (true)
+		{
+			/* code */
+			cout << "saisissez la requete : " << endl;
+			string requete;
+			getline(cin, requete);
+			const char* requeteEnv = requete.c_str();
+			
+			Write(pipefd1[1], requeteEnv, 7);
+		}
+		
+	
+	}
+	else{ // dans le fils
+		Close(pipefd1[1]);
+		Read(pipefd1[0], requeteRecue, Nbuff);
+		cout << getppid() << endl;
+		cout << requeteRecue << endl;
+ 		kill(getppid(),SIGUSR2);
+	}
+	
+	
+
+}
+  catch (const CExc & Exc) {
+        cerr <<Exc<< endl;
+        return errno;
+  }
+  catch (const exception & Exc) {
+        cerr << "Exception : " << Exc.what () << endl;
+        return 1;
+  }
+  catch (...) {
+        cerr << "Exception inconnue recue dans la fonction main()" 
+             << endl;
+        return 1;
+  }
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
